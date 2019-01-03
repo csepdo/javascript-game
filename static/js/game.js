7b457d7edbe7ff3d13acc4ae05819096f29b9e65
csepdo <arodzeta@gmail.com>
2019-01-03 23:39:21 +0100

    ...

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,5 @@
+let score = 0;
+
 function collapsibleButtons() {
     let coll = document.getElementsByClassName("collapsible");
     let i;
@@ -15,39 +17,81 @@ function collapsibleButtons() {
     }
 }
 
+
 function showCheckWord(status, side) {
     if (status === 'correct') {
-        let owl_picture = document.getElementsByClassName('owl')
-        owl_picture.setAttribute('src', 'static/img/sad_owl.png')
+        if (side === 'left') {
+            changePicture('.owl', 'static/img/happy_owl.png');
+            setTimeout(function () {
+                changePicture('.owl', 'static/img/wise_owl.png');
+            }, 1500)
+        }
+        else if (side === 'right') {
+            changePicture('.parrot', 'static/img/happy_parrot.png');
+            setTimeout(function () {
+                changePicture('.parrot', 'static/img/greeting_parrot.png');
+            }, 1500)
+        }
+        score++;
     }
     else if (status === 'incorrect') {
+        if (side === 'left') {
+            changePicture('.owl', 'static/img/sad_owl.png');
+            setTimeout(function () {
+                changePicture('.owl', 'static/img/wise_owl.png');
+            }, 1500);
 
+        }
+        else if (side === 'right') {
+            changePicture('.parrot', 'static/img/sad_parrot.png');
+            setTimeout(function () {
+                changePicture('.parrot', 'static/img/greeting_parrot.png');
+            }, 1500)
+        }
     }
 }
 
-function checkWord(xhttp, word) {
+
+function changePicture(element, url) {
+    $ (element).attr('src', url);
+}
+
+
+function checkWord(xhttp, word, side) {
     let word_list = xhttp.responseText;
     if (word_list.search(word.textContent) >= 0) {
-        console.log('correct');
-        showCheckWord('correct');
+        if (side === 'left') {
+            console.log('correct');
+            showCheckWord('correct', 'left');
+        }
+        else if (side === 'right') {
+            showCheckWord('correct', 'right');
+        }
     } else {
         console.log('incorrect');
-        word.classes.add('.missed')
-        showCheckWord('incorrect');
+        word.classList.add('missed');
+        if (side === 'left') {
+            showCheckWord('incorrect', 'left');
+        }
+        else if (side === 'right') {
+            showCheckWord('incorrect', 'right');
+        }
     }
 
 }
 
-function loadDoc(filename, word) {
+
+function loadDoc(filename, word, side) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            checkWord(this, word)
+            checkWord(this, word, side)
         }
     };
     xhttp.open("GET", filename, true);
     xhttp.send();
 }
+
 
 function dragAndDrop() {
     function $(id) {
@@ -57,15 +101,22 @@ function dragAndDrop() {
         revertOnSpill: true
     }).on('drop', function (el) {
         let word = el;
+        let side;
         if (el.parentElement === $('drop-target-left')) {
-            loadDoc('/static/data/LY.txt', word)
+            side = 'left';
+            loadDoc('/static/data/LY.txt', word, side)
         }
         else if (el.parentElement === $('drop-target-right')) {
-            loadDoc('/static/data/J.txt', word)
+            side = 'right';
+            loadDoc('/static/data/J.txt', word, side)
         }
     });
 }
 
+
+function showScore () {
+
+}
 
 collapsibleButtons();
 dragAndDrop();
