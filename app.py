63f6cd7c647e9eac7bd29ea830dbf86e9044c6d1
csepdo <arodzeta@gmail.com>
2019-01-03 17:07:01 +0100

    Preparing for Heroku deploy.

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
 from flask import Flask, render_template, request
 import data_manager
-import json
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def hello_world():
     return render_template('index.html')
 
 
-@app.route('/jatek', methods=["GET"])
+@app.route('/jatek')
 def play_game():
     word_number = int(request.args.get('word-number', 10))
     selected_words = data_manager.get_random_words(word_number)
-    words_ly = json.dumps(data_manager.get_words_from_file('LY.txt'))
-    words_j = json.dumps(data_manager.get_words_from_file('J.txt'))
-    return render_template('game.html', words=selected_words, words_ly=words_ly, words_j=words_j)
+    return render_template('game.html', words=selected_words)
 
 
 @app.route('/szavak')
 def show_words():
-    words = {'words_ly': data_manager.get_words_for_game_json("LY.txt"),
-             'words_j': data_manager.get_words_for_game_json("J.txt")}
-    return render_template('words.html', words_ly=words['words_ly'], words_j=words['words_j'])
+    words_ly = data_manager.get_words_for_game("LY.txt")
+    words_j = data_manager.get_words_for_game("J.txt")
+    return render_template('words.html', words_ly=words_ly, words_j=words_j)
 
 
 if __name__ == '__main__':
